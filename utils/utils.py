622c266a7095b10270ee2b8defdd3bf35769e28e
Naveen jangra <90304808+Nav-jangra@users.noreply.github.com>
2025-01-18 21:11:16 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -138,7 +138,9 @@ def LoadDriver(headless):
     if headless:
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("headless")
-
+        options.add_argument('--no-sandbox')
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        
     # updated to use ChromeDriverManager to match correct chromedriver automatically
     driver = None
     try:
